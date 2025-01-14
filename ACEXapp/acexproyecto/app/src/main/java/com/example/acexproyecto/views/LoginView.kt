d6ff724b348c0979c0ee08d777482baa6d8c6c7d
Santiago Manuel Tamayo Arozamena <145398719+DAMSanti@users.noreply.github.com>
2025-01-14 19:53:34 +0100

    Login MSAL y credenciales

--- ACEXapp/acexproyecto/app/src/main/java/com/example/acexproyecto/views/LoginView.kt
+++ ACEXapp/acexproyecto/app/src/main/java/com/example/acexproyecto/views/LoginView.kt
@@ -156,137 +156,134 @@ fun LoginView(navController: NavController) {
                 color = MaterialTheme.colorScheme.primary
             )
         } else {
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            // Logo
-            Image(
-                painter = painterResource(id = R.drawable.logorecortado), // Asegúrate de tener el logo en la carpeta drawable
-                contentDescription = "Logo",
+            Column(
                 modifier = Modifier
-                    .size(300.dp)
-                    .padding(bottom = 0.dp) // Espaciado debajo del logo
-            )
-
-            // Título de Login
-            Text(
-                text = "Login",
-                fontSize = 30.sp,
-                color = TextPrimary, // Color del texto principal
-                modifier = Modifier.padding(bottom = 32.dp)
-            )
-
-            // Campo de texto para el nombre de usuario con ícono
-            OutlinedTextField(
-                value = username,
-                onValueChange = { username = it },
-                label = { Text("Usuario", color = TextPrimary) },
-                modifier = Modifier.fillMaxWidth(),
-                singleLine = true,
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    focusedBorderColor = Accent, // Color del borde cuando está enfocado
-                    unfocusedBorderColor = ButtonPrimary, // Color del borde cuando no está enfocado
-                    focusedLabelColor = Accent, // Color de la etiqueta enfocada
-                    unfocusedLabelColor = TextPrimary, // Color de la etiqueta no enfocada
-                    cursorColor = TextPrimary // Color del cursor
-                ),
-                leadingIcon = {
-                    Image(
-                        imageVector = Icons.Filled.Person, // Ícono de usuario
-                        contentDescription = "User Icon",
-                        modifier = Modifier.size(24.dp),
-                        colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(TextPrimary)
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Campo de texto para la contraseña con ícono
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("Contraseña", color = TextPrimary) },
-                modifier = Modifier.fillMaxWidth(),
-                singleLine = true,
-                visualTransformation = PasswordVisualTransformation(),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    focusedBorderColor = Accent,
-                    unfocusedBorderColor = ButtonPrimary,
-                    focusedLabelColor = Accent,
-                    unfocusedLabelColor = TextPrimary,
-                    cursorColor = TextPrimary // Color del cursor
-                ),
-                leadingIcon = {
-                    Image(
-                        imageVector = Icons.Filled.Lock, // Ícono de candado
-                        contentDescription = "Password Icon",
-                        modifier = Modifier.size(24.dp),
-                        colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(TextPrimary)
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-
-            // Botón de inicio de sesión
-            Button(
-                onClick = { navController.navigate("home") },
-                modifier = Modifier
-                    .fillMaxWidth()  // Asegura que el botón ocupe to do el ancho disponible
-                    .height(60.dp),  // Ajusta la altura del botón
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = ButtonPrimary // Color del botón
-                )
+                    .fillMaxSize()
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = "Iniciar Sesión",
-                    fontSize = 24.sp,  // Aumenta el tamaño de la fuente
-                    color = Color.White
-                )
-            }
-
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Botón de Microsoft con su logo
-            Button(
-                onClick = {
-                    // Aquí rediriges al usuario al flujo de inicio de sesión de Microsoft
-                    showLoginDialog()
-                },
-                modifier = Modifier
-                    .fillMaxWidth(0.6f)
-                    .align(Alignment.CenterHorizontally)
-                    .padding(top = 16.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Accent // Color para Microsoft
-                )
-            ) {
+                // Logo
                 Image(
-                    painter = painterResource(id = R.drawable.images), // Logo de Microsoft
-                    contentDescription = "Microsoft Logo",
-                    modifier = Modifier.size(24.dp)
+                    painter = painterResource(id = R.drawable.logorecortado), // Asegúrate de tener el logo en la carpeta drawable
+                    contentDescription = "Logo",
+                    modifier = Modifier
+                        .size(300.dp)
+                        .padding(bottom = 0.dp) // Espaciado debajo del logo
                 )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "Iniciar sesión con Microsoft", fontSize = 16.sp, color = Color.White)
-            }
 
-            Spacer(modifier = Modifier.height(16.dp))
+                // Título de Login
+                Text(
+                    text = "Login",
+                    fontSize = 30.sp,
+                    color = TextPrimary, // Color del texto principal
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
 
-            // Botón de "¿Olvidaste la contraseña?"
-            TextButton(
-                onClick = { navController.navigate("reset_password") }, // Redirige a recuperación de contraseña
-                modifier = Modifier.padding(top = 16.dp)
-            ) {
-                Text(text = "He olvidado la contraseña", color = Accent)
+                // Campo de texto para el nombre de usuario con ícono
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = { username = it },
+                    label = { Text("Usuario", color = TextPrimary) },
+                    modifier = Modifier.fillMaxWidth(),
+                    singleLine = true,
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Accent, // Color del borde cuando está enfocado
+                        unfocusedBorderColor = ButtonPrimary, // Color del borde cuando no está enfocado
+                        focusedLabelColor = Accent, // Color de la etiqueta enfocada
+                        unfocusedLabelColor = TextPrimary, // Color de la etiqueta no enfocada
+                        cursorColor = TextPrimary // Color del cursor
+                    ),
+                    leadingIcon = {
+                        Image(
+                            imageVector = Icons.Filled.Person, // Ícono de usuario
+                            contentDescription = "User Icon",
+                            modifier = Modifier.size(24.dp),
+                            colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(TextPrimary)
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Campo de texto para la contraseña con ícono
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Contraseña", color = TextPrimary) },
+                    modifier = Modifier.fillMaxWidth(),
+                    singleLine = true,
+                    visualTransformation = PasswordVisualTransformation(),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Accent,
+                        unfocusedBorderColor = ButtonPrimary,
+                        focusedLabelColor = Accent,
+                        unfocusedLabelColor = TextPrimary,
+                        cursorColor = TextPrimary // Color del cursor
+                    ),
+                    leadingIcon = {
+                        Image(
+                            imageVector = Icons.Filled.Lock, // Ícono de candado
+                            contentDescription = "Password Icon",
+                            modifier = Modifier.size(24.dp),
+                            colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(TextPrimary)
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+
+                // Botón de inicio de sesión
+                Button(
+                    onClick = { navController.navigate("home") },
+                    modifier = Modifier
+                        .fillMaxWidth()  // Asegura que el botón ocupe to do el ancho disponible
+                        .height(60.dp),  // Ajusta la altura del botón
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = ButtonPrimary // Color del botón
+                    )
+                ) {
+                    Text(
+                        text = "Iniciar Sesión",
+                        fontSize = 24.sp,  // Aumenta el tamaño de la fuente
+                        color = Color.White
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Botón de Microsoft con su logo
+                Button(
+                    onClick = {
+                        // Aquí rediriges al usuario al flujo de inicio de sesión de Microsoft
+                        showLoginDialog()
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(0.6f)
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 16.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Accent // Color para Microsoft
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.images), // Logo de Microsoft
+                        contentDescription = "Microsoft Logo",
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = "Iniciar sesión con Microsoft", fontSize = 16.sp, color = Color.White)
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Botón de "¿Olvidaste la contraseña?"
+                TextButton(
+                    onClick = { navController.navigate("reset_password") }, // Redirige a recuperación de contraseña
+                    modifier = Modifier.padding(top = 16.dp)
+                ) {
+                    Text(text = "He olvidado la contraseña", color = Accent)
+                }
             }
         }
-    }
-
     }
 }
 
